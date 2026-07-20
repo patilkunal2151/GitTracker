3e22c910734a4c14dd3cd7dd21375ac0777a60bd
patilkunal2151 <kp6945701@gmail.com>
2026-07-20 08:43:12 +0530

    Bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.gittracker"
         minSdk = 29
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resConfigs("en")
